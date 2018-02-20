180e16a589e128fe91d391943406e7b699c7327c
japicazos <ja.picazos.carrillo@gmail.com>
2018-02-20 22:43:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api'); 
-const token = '526659234:AAGylJPlU_qnWA3tOFGV1YMtWxxvwYBWQ8w';
+const token = '';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
