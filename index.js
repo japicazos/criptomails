0aa5cc804e48d7b676cab81e701dc1c147e2bc7f
japicazos <ja.picazos.carrillo@gmail.com>
2018-02-20 22:41:29 +0100

    Entrar

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api'); 
 const token = '526659234:AAGylJPlU_qnWA3tOFGV1YMtWxxvwYBWQ8w';
 const bot = new TelegramBot(token, {polling: true});
@@ -6,5 +7,11 @@ bot.on('message', (msg) => {
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
         bot.sendMessage(msg.chat.id,"Hello dear user");
-    }         
+    }   
+    
+    var bye = "bye";
+    if (msg.text.toString().toLowerCase().includes(bye)) {
+        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+    } 
 });
+
